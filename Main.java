d1f305f1b1d48462eaeed00336b6b39630321304
Nengyi Jonathan Jiang <theNerdyMango@gmail.com>
2022-02-22 11:53:10 -0600

    .

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import javax.swing.JFrame;
+
+/**
+ * This file is complete. You don't need to do anything to it, but
+ * it is where you'll run your project when you're ready to see
+ * your maze. 
+ */
+public class Main extends JFrame {
+
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 1000;
+
+    public Main() {
+        super("Maze Builder");
+        setSize(WIDTH, HEIGHT);
+        // getContentPane().add(
+
+        // ); 
+        setVisible(true); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        new Main(); 
+    }
 }
